8704794ab023afbe1564a72a4cd9ad6c31a46a42
dare_falola <darefalola7@gmail.com>
2020-04-30 19:12:29 +0100

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,34 @@
-// Global app controller
+import Search from "./models/Search";
+import { elements } from "./views/base";
+import * as searchView from "./views/searchView";
+/** Global state of the app
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes
+ */
+const state = {};
+
+const controlDefault = async () => {
+  // Get query from view
+  const query = searchView.getInput();
+  if (query) {
+    // New search object added to state
+    state.search = new Search(query);
+
+    // Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+    // Search for recipes
+    await state.search.getResults();
+
+    // Render results
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlDefault();
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  // mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  // Output of the HTML file
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  // Babel loader for converting js files to es5
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
